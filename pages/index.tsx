0f049f20e19ff4418002ccbaaa35d952abcb81c6
peterbang <peter.bang@social-chart.com>
2022-06-02 23:38:54 +0900

    next-auth with provider google configured

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import LoginBtn from "../components/login-btn";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <LoginBtn />
+    </div>
+  );
+}
